ab37a4d30fa9a9dc129e393b20c3a61476a7dde5
E. K. Lee <wolf@teoal.net>
2026-01-07 02:43:25 +0900

    react album 첫 화면 실습 중

---
+++ react-album/src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/index.module.scss'
+
+function index() {
+  return (
+    <div className={styles.page}>
+      <h1>일단 메인 페이지...</h1>
+    </div>
+  )
+}
+
+export default index
