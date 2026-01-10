97ba91f240ad5d98ef7aebc7c58dc516432a9656
E. K. Lee <wolf@teoal.net>
2026-01-11 02:16:10 +0900

    react blog - Header Footer 작업중

--- react-blog/src/App.tsx
+++ react-blog/src/App.tsx
@@ -1,5 +1,14 @@
+import {AppFooter, AppHeader} from "@/components";
+
 export default function App() {
+
   return (
-    <div>Contents from App.tsx</div>
+    <div className="page">
+      <AppHeader />
+      <div className="container">
+        <p style={{display: 'inline-block', width: '1200', background: "red"}}>여기가 내용이고, flex colum 방향으로 가운데 정렬, 최대 넓이는 1328px</p>
+      </div>
+      <AppFooter />
+    </div>
   )
 }
